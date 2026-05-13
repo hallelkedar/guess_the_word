82b9258ea03d135ce60a7668931508dd5d1d632e
Hallel <hallelkedar000@gmail.com>
2026-05-13 13:20:32 +0300

    Add main file - play the game

--- main.py
+++ main.py
@@ -1,22 +1,10 @@
-from words import get_random_word
-from game import get_the_word
-
-GUESSES_LIMIT = 7
-
-MENU = """
-======= Welcome to Guess The Word ! ======
-            1. Start Game (random category)
-            2. Choose Category
-            3. Exit
-    """
-
-user_attemps = 0
-word_disable = True
-wrong_letter = []
-
+from game import game_loop
 
 def main():
-    word = get_the_word(MENU, GUESSES_LIMIT)
+    while True:
+        choice = game_loop()
 
+        if choice == 'exit':
+            return
 if __name__ == '__main__':
     main()
