7eb6d3bdf6dada7eba5d38b390283fdee230a3dd
Hallel <hallelkedar000@gmail.com>
2026-05-13 09:25:34 +0300

    Add getter words func

--- words.py
+++ words.py
@@ -11,7 +11,7 @@ def get_word_dict(file_path: Path) -> dict:
     Gets file path
     and return dictionary of categories and their word list
     """
-    word_dict = {}
+    words_dict = {}
     current_category = None
 
     with open(file_path) as f:
@@ -23,21 +23,21 @@ def get_word_dict(file_path: Path) -> dict:
 
             elif line.endswith("#"):
                     current_category = line[1:].strip()
-                    word_dict[current_category] = []
+                    words_dict[current_category] = []
 
             else:
-                word_dict[current_category] = line
-    return word_dict
+                words_dict[current_category] = line
+    return words_dict
 
-def get_category_lst(word_dict):
+def get_category_lst(words_dict):
     category_lst = []
     
-    for category in word_dict:
+    for category in words_dict:
         category_lst.append(category)
     return category_lst
 
-def get_random_word(word_dict, category):
-    word_lst = word_dict[category]
-    random_index = random.randint(0, (len(word_lst)-1))
+def get_random_word(category):
     
-    return word_lst[random_index]
+    words_dict = get_word_dict(file_path)
+    word_lst = words_dict[category]
+    return random.choice(word_lst)
